10cfdf41aeacab051d30d0518cca63cf7817e7a5
David León V <datevid@gmail.com>
2023-06-12 23:52:56 -0500

    Update equigroup01.py

--- AppRoot/model/equigroup01.py
+++ AppRoot/model/equigroup01.py
@@ -5,9 +5,13 @@ jugadores = [
     {'nombre': 'Jugador2', 'peso': 2},
     {'nombre': 'Jugador3', 'peso': 3},
     {'nombre': 'Jugador4', 'peso': 4},
+    {'nombre': 'Jugador5', 'peso': 5},
+    {'nombre': 'Jugador6', 'peso': 6},
 ]
 
-combinaciones_jugadores = list(itertools.combinations(jugadores, 2))
+combinaciones_jugadores = list(itertools.combinations(jugadores, 3))
+print("combinaciones_jugadores:")
+print(combinaciones_jugadores)
 #---------------------------------
 #impresión
 i=0;
@@ -19,7 +23,9 @@ print(f"{len(combinaciones_jugadores)} Equipo encontrados")
 
 equipos = [f'Equipo{i}' for i in range(1, len(combinaciones_jugadores) + 1)]
 print("equipos")
-print(equipos)
+print(equipos,type(equipos))
+print("equipo.index(0)")
+print(equipos.index("Equipo1"))
 
 enfrentamientos = list(itertools.combinations(equipos, 2))
 print("enfrentamientos")
@@ -35,4 +41,6 @@ for enfrentamiento in enfrentamientos:
     
     #si algún jugador del equipo 1 está presente en el equipo 2
     if not any(jugador_nombre in jugadores2_nombres for jugador_nombre in jugadores1_nombres):
-        print(f"{equipo1} vs {equipo2}")
+        peso_equipo1 = sum(jugador['peso'] for jugador in jugadores_equipo1)
+        peso_equipo2 = sum(jugador['peso'] for jugador in jugadores_equipo2)
+        print(f"{equipo1} (Peso: {peso_equipo1}) vs {equipo2} (Peso: {peso_equipo2})")
